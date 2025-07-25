654344c6149be226ae169fc411aa3b0d3d495212
Syedmusharaf24 <syedmusharaf2462002@gmaail.com>
2025-07-25 12:11:42 +0530

    Merge branch 'main' of https://github.com/syedmusharaf24/googlereview_customer_insights

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 import streamlit as st
-from google.oauth2 import service_account
-
-
-credentials = service_account.Credentials.from_service_account_info(st.secrets["google"])
-
 import pandas as pd
 import numpy as np
 import plotly.express as px
